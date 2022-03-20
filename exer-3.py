c942eaf7d3a4541d9010f52d7bb0aee8db4de3d7
LisandroCazuza <geohist2005@gmail.com>
2022-03-20 17:10:59 -0300

    EstruturaDecisao

--- exer-3.py
+++ exer-3.py
@@ -1,6 +1,12 @@
 """   3.Faça um Programa que verifique se uma letra digitada é "F" ou "M" ou "H". 
 Conforme a letra escrever: F - Feminino, M - Masculino ou H - Homossexual"""
 
-sexo = input("Digite sua sexualidade: ").title()
-if sexo == 'F/ f ':
-    print(sexo)
+sexualidade = input("Digite sua sexualidade -F - Feminino, M - Masculino ou H - Homossexual - :  ").title()
+if sexualidade == 'F':
+    print(f" Foi digitado  {sexualidade} portanto você é feminina.")
+if sexualidade == 'M':
+    print(f" Foi digitado  {sexualidade} portanto você é masculino.")
+if sexualidade == 'H':
+    print(f" Foi digitado  {sexualidade} portanto você é homossexual.")
+else:
+    print("Não foi digitado nenhuma das opções. Desta forma não há regitro de sexualidade")
