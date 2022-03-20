a597e02ec79c32a732a88b9f0505ce6c76ff444f
LisandroCazuza <geohist2005@gmail.com>
2022-03-20 16:27:22 -0300

    EstruturaDecisao

--- exer-1.py
+++ exer-1.py
@@ -4,7 +4,7 @@ n2 = input("Digite o segundo número: ")
 if n1 > n2:
     print(f"O número N1 {n1} é maior que o número  N2 {n2}.")
 if n2 > n1: 
-    print(f"O número N2 {n2} é maior que o número  N1 {n1}".)
-else:
+    print(f"O número N2 {n2} é maior que o número  N1 {n1}.")
+if n1 == n2:
     print("Os dois números são iguais.")
     
