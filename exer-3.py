2d175c2687b49ea7148c7a673ebd22103f6f7969
LisandroCazuza <geohist2005@gmail.com>
2022-03-20 16:55:43 -0300

    EstruturaDecisao

---
+++ exer-3.py
@@ -0,0 +1,6 @@
+"""   3.Faça um Programa que verifique se uma letra digitada é "F" ou "M" ou "H". 
+Conforme a letra escrever: F - Feminino, M - Masculino ou H - Homossexual"""
+
+sexo = input("Digite sua sexualidade: ").title()
+if sexo == 'F/ f ':
+    print(sexo)
