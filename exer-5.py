4c1f00b8852135a6a1cd008b3b5314effa2fed2a
LisandroCazuza <geohist2005@gmail.com>
2022-03-27 12:54:34 -0300

    EstruturaDecisao

--- exer-5.py
+++ exer-5.py
@@ -3,3 +3,6 @@ O programa deve calcular a média alcançada por aluno e apresentar:
 > A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 > A mensagem "Reprovado", se a média for menor do que sete;
 > A mensagem "Aprovado com Distinção", se a média for igual a dez.   """
+print("-"*60)
+nota1 = foat(input("Digite a primira nota: "))
+
