667acbefa4c63ac5f5873963b0442f542fb17304
LisandroCazuza <geohist2005@gmail.com>
2022-03-27 15:13:10 -0300

    EstruturaDecisao

--- exer-6.py
+++ exer-6.py
@@ -1,9 +1,13 @@
 """ Faça um Programa que leia três números e mostre o maior deles.  """
+print("-"*50)
 n1 =  int(input("Digite o primeiro número: ")) 
 n2 =  int(input("Digite o segundo número: "))
-if n1 > n2:
-    print(f"O número n1 {n1} é maior que n2 {n2}. ")
-if n2 > n1:
-   print(f"O número n2 {n2} é maior que n1 {n1}. ") 
-if : 
-    print("Os números digitados são iguais.")
+n3=  int(input("Digite o terceiro número: "))
+if n1 > n2 and n1  > n3:
+    print(f"O número n1: {n1} é maior que n2: {n2} e n3: {n3}. ")
+if n2 > n1 and n2 > n3:
+   print(f"O número n2: {n2} é maior que n1: {n1} e n3: {n3}. ") 
+if n3 > n1 and n3 > n2:
+       print(f"O número n3: {n3} é maior que n1: {n1} e n2: {n2}. ") 
+else:
+    print("Reveja os números digitados. Ocorreu um erro!")
