d3d14877e170603a1a8dd4f1c3b3aca4a612a50d
LisandroCazuza <geohist2005@gmail.com>
2022-03-27 17:12:15 -0300

    EstruturaDecisao

--- exer-10.py
+++ exer-10.py
@@ -2,4 +2,12 @@
 Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a
 mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso."""
 print("-"*50)
-turno = input("Em qual turno você estuda?  ")
+turno = input("Em qual turno você estuda?  ").title()
+if turno == 'M':
+    print("Bom Dia! Estudante!")
+if turno == 'V':
+    print("Boa Tarde! Estudante!")
+    if turno == 'N':
+        print("Boa Noite! Estudante!")
+    else:
+        print("Valor Inválido!Estudante!")
