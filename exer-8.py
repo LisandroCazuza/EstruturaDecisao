6a2c56b38feeace86d24669e4e70b5aba5075002
LisandroCazuza <geohist2005@gmail.com>
2022-03-27 15:48:04 -0300

    EstruturaDecisao

--- exer-8.py
+++ exer-8.py
@@ -1,2 +1,15 @@
 """Faça um programa que pergunte o preço de três produtos e informe
 qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato."""
+print("-" * 50)
+p1 = input("Digite o nome do produto: ")
+n1 = int(input("Digite o primeiro valor: "))
+p2 = input("Digite o nome do produto: ")
+n2 = int(input("Digite o segundo valor: "))
+p3 = input("Digite o nome do produto: ")
+n3 = int(input("Digite o terceiro valor: "))
+if n1 < n2 and n1 < n3:
+    print(f"O valor do produto 1:  R$ {n1} {p1}. ")
+if n2 < n1 and n2 < n3:
+    print(f"O valor do produto 2:  R$ {n2} {p2}. ")
+if n3 < n1 and n3 < n2:
+    print(f"O valor do produto 3:  R$ {n3} {p2}. ")
