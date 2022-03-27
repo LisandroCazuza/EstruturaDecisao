7c945f0252410f20e9b0cf2d516ed049168b83ea
LisandroCazuza <geohist2005@gmail.com>
2022-03-27 14:05:13 -0300

    EstruturaDecisao

--- exer-6.py
+++ exer-6.py
@@ -5,5 +5,5 @@ if n1 > n2:
     print(f"O número n1 {n1} é maior que n2 {n2}. ")
 if n2 > n1:
    print(f"O número n2 {n2} é maior que n1 {n1}. ") 
-else:
+if : 
     print("Os números digitados são iguais.")
