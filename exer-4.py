578a86b3b121ed15be5b7a13be7222ec7d456e27
LisandroCazuza <geohist2005@gmail.com>
2022-03-27 12:08:13 -0300

    EstruturaDecisao

--- exer-4.py
+++ exer-4.py
@@ -1,1 +1,6 @@
 """  4.Faça um Programa que verifique se uma letra digitada é vogal ou consoante.  """
+vogal = ['a','e','i','o','u','A', 'E','I','O','U']
+v = input("Digite uma letra para saber se é uma vogal ou consoante: ")
+
+
+ 
