9c0ca14c6d623151b8a060a60fe11727771b8d77
LisandroCazuza <geohist2005@gmail.com>
2022-04-17 14:40:12 -0300

    EstruturaDecisao

---
+++ exer -18.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que peça uma data no formato dd/mm/aaaa e 
+determine se a mesma é uma data válida.    """
+print("--------------------------------------------------------------------------------------------------------")
+dia = int(input("Digite o dia do ano : "))
+mes = int(input("Digite o mês do ano: "))
+ano = int(input("Digite o ano: "))
+data = (f"{dia} / {mes} / {ano} ")
+print(data)
